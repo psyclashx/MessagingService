15c125c7809ed689330ddb2f6cbfe210cc3e5ba5
psyclashx <flo-kraus1@web.de>
2023-05-30 16:47:27 +0200

    Final Commit

--- src/Callback/MessagingService.java
+++ src/Callback/MessagingService.java
@@ -1,14 +1,35 @@
 package Callback;
 
+import java.io.SyncFailedException;
 import java.rmi.RemoteException;
+import java.util.Random;
 
 public class MessagingService implements MessagingServiceIF {
     @Override
     public void sendMessage(Message message, CallbackIF callback) throws RemoteException {
 
-        System.out.println(message.toString());
 
-        callback.nachrichtZustellen(message, callback);
+        Runnable serviceTast = new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    System.out.println(message.toString());
+                    System.out.println("/METHOD INVOCATION 'sendMessage' executed");
+
+                    Thread.sleep(5000);
+
+                    callback.nachrichtZustellen(message, callback);
+                } catch (InterruptedException | RemoteException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        new Thread(serviceTast).start();
+        return;
+
+
 
     }
 }
